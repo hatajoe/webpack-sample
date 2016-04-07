e82b873a63e89ad4df8491502fb2a1acb174079f
Yusuke Hatanaka <arbalestimp@gmail.com>
2016-04-08 00:22:21 +0900

    Support css sprite

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
+var sprite = require('sprite-webpack-plugin');
+
+var path = {
+  src: "./src",
+  dest: "./public/assets" 
+};
+
 module.exports = {
   entry: {
-    index: "./src/js/index.js",
-    campaign1: "./src/js/campaign1.js",
-    campaign2: "./src/js/campaign2.js"
+    index: path.src + "/js/index.js",
+    campaign1: path.src + "/js/campaign1.js",
+    campaign2: path.src + "/js/campaign2.js"
   },
   output: {
-    path: './public/assets/js',
+    path: path.dest + '/js',
     publicPath: '/assets/js/',
     filename: "[name].js"
   },
@@ -33,5 +40,12 @@ module.exports = {
   },
   eslint: {
     configFile: './.eslintrc'
-  }
+  },
+  plugins: [
+    new sprite({
+      'source' : path.src + '/img/sprite/',
+      'imgPath': path.dest + '/img/',
+      'cssPath': path.src + '/css/'
+    })
+  ]
 };
